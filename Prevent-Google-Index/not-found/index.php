bd308800559848ad618507ddb24ad41d068f4174
Fedmich <fedmich@gmail.com>
2015-08-07 13:43:49 +0800

    Basic page to display

---
+++ Prevent-Google-Index/not-found/index.php
@@ -0,0 +1,16 @@
+<?php
+	
+	?><html>
+<head>
+	<meta name="googlebot-news" content="noindex" />
+	<meta name="ROBOTS" content="NOINDEX,NOFOLLOW" />
+	<title>Sorry, Page not found</title>
+	
+</head>
+<body>
+	<h1>Sorry, Page not found</h1>
+	<p>
+	We apologize but this page is not available. Either the page is not found or it has been removed.
+	</p>
+</body>
+</html>
